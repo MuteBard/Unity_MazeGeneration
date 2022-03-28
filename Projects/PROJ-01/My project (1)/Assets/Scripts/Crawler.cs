0a6c911e86d7ddbbbcf9f5904b79c5b920895437
Mutebard <csevere@brandeis.edu>
2022-03-28 09:13:44 -0400

    DEV-04: multiple crawlers

--- Projects/PROJ-01/My project (1)/Assets/Scripts/Crawler.cs
+++ Projects/PROJ-01/My project (1)/Assets/Scripts/Crawler.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Crawler : MazeGen
 {
-
     public override void Generate(){
+        CrawlZ();
+        CrawlX();
+        CrawlRandom();
+        CrawlX();
+    }
+    public void CrawlRandom(){
         bool done = false;
         int x = width / 2;
         int z = depth / 2;
@@ -14,6 +19,48 @@ public class Crawler : MazeGen
             map[x, z] = 0;
             int nx = UnityEngine.Random.Range(-1, 2);
             int nz = UnityEngine.Random.Range(-1, 2);
+
+            if(Random.Range(0,100) < 50) {
+                x += nx;
+            }else{
+                z += nz;
+            }
+            
+            // crawl until you hit the edge of the map
+            done = ( x < 0 || x >= width || z < 0 || z >= depth );
+        } 
+    }
+
+    public void CrawlX(){
+        bool done = false;
+        int x = 0;
+        int z = depth / 2;
+
+        while (!done){
+            map[x, z] = 0;
+            int nx = UnityEngine.Random.Range(0, 2);
+            int nz = UnityEngine.Random.Range(-1, 2);
+
+            if(Random.Range(0,100) < 50) {
+                x += nx;
+            }else{
+                z += nz;
+            }
+            
+            // crawl until you hit the edge of the map
+            done = ( x < 0 || x >= width || z < 0 || z >= depth );
+        } 
+    }
+
+        public void CrawlZ(){
+        bool done = false;
+        int x = width / 2;
+        int z = 0;
+
+        while (!done){
+            map[x, z] = 0;
+            int nx = UnityEngine.Random.Range(-1, 2);
+            int nz = UnityEngine.Random.Range(0, 2);
 
             if(Random.Range(0,100) < 50) {
                 x += nx;
